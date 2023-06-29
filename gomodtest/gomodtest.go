1d5aa463c4c2e6249201786e9262545c0e6dc2fc
jsoncshong <jsoncshong@tencent.com>
2023-06-29 14:48:17 +0800

    update

--- gomodtest/gomodtest.go
+++ gomodtest/gomodtest.go
@@ -3,6 +3,6 @@ package gomodtest
 import "fmt"
 
 func Hello() {
-	//fmt.Println("@@@@@@@@@@@ master hello 1")
-	fmt.Println("@@@@@@@@@@@ tag v2.0.3 hello 1")
+	fmt.Println("@@@@@@@@@@@ master hello 2")
+	//fmt.Println("@@@@@@@@@@@ tag v2.0.3 hello 1")
 }
